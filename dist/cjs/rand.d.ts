db02824d5b9d61c52a84c9c7aabc8061be0b251e
tom <toms@live.jp>
2022-07-18 06:08:04 +0900

    dist directory is ignored

--- dist/cjs/rand.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class Rand {
-    constructor();
-    static char(): string;
-    static str(length?: number): string;
-    static id(prefix_str?: string): string;
-}
-export default Rand;
