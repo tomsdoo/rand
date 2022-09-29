88483fbf85243c8295d79f3c559aef7c98ee49d4
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-09-29 19:13:06 +0900

    prettier is added (#28)
    * prettier is installed
    
    * npm script format:src is added
    
    * format:src
    
    * npm script format:test is added
    
    * format:test
    
    * npm script format is added

--- src/rand.ts
+++ src/rand.ts
@@ -2,32 +2,35 @@
  * @license rand
  * (c) 2019 tom
  * License: MIT
-*/
-function range(n : number,s ?: number) : number[] {
-  let r : number[] = [];
+ */
+function range(n: number, s?: number): number[] {
+  let r: number[] = [];
   let ls = s ? s : 0;
-  for(let i = 0; i < n; i++){
-    r.push(i +ls);
+  for (let i = 0; i < n; i++) {
+    r.push(i + ls);
   }
   return r;
 }
 
 export class Rand {
-  constructor(){
+  constructor() {}
+  public static char() {
+    return String.fromCharCode(
+      "a".charCodeAt(0) + Math.floor(Math.random() * 26)
+    );
   }
-  public static char(){
-    return String.fromCharCode(("a").charCodeAt(0) + Math.floor(Math.random() * 26));
+  public static str(length?: number) {
+    return range(length ? length : 5)
+      .map(Rand.char)
+      .join("");
   }
-  public static str(length ?: number){
-    return range(length ? length : 5).map(Rand.char).join("");
-  }
-  public static id(prefix_str ?: string){
+  public static id(prefix_str?: string) {
     return [
-      (prefix_str ? prefix_str : ""),
+      prefix_str ? prefix_str : "",
       Rand.str(),
-      (new Date()).getTime().toString()
+      new Date().getTime().toString(),
     ].join("");
   }
-};
+}
 
 export default Rand;
