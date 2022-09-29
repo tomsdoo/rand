cab5b03cc0c2090b24b187e0f0bb9917a8c4b291
tom <toms@live.jp>
2022-09-30 05:56:39 +0900

    lint:src fix

--- src/rand.ts
+++ src/rand.ts
@@ -4,29 +4,31 @@
  * License: MIT
  */
 function range(n: number, s?: number): number[] {
-  let r: number[] = [];
-  let ls = s ? s : 0;
+  const r: number[] = [];
+  const ls = s ?? 0;
   for (let i = 0; i < n; i++) {
     r.push(i + ls);
   }
   return r;
 }
 
+// eslint-disable-next-line
 export class Rand {
-  constructor() {}
-  public static char() {
+  public static char(): string {
     return String.fromCharCode(
       "a".charCodeAt(0) + Math.floor(Math.random() * 26)
     );
   }
-  public static str(length?: number) {
-    return range(length ? length : 5)
+
+  public static str(length?: number): string {
+    return range(length ?? 5)
       .map(Rand.char)
       .join("");
   }
-  public static id(prefix_str?: string) {
+
+  public static id(prefixStr?: string): string {
     return [
-      prefix_str ? prefix_str : "",
+      prefixStr ?? "",
       Rand.str(),
       new Date().getTime().toString(),
     ].join("");
